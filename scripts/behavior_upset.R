30ded049fa8bc9714f33525481464422414c77a7
Grace Kenney <gracekenney@Graces-MBP-226.lan>
2024-12-07 01:27:19 -0500

    add activity bars and patch both plots together

--- scripts/behavior_upset.R
+++ scripts/behavior_upset.R
@@ -1,9 +1,25 @@
 library(ggupset)
 library(tidyverse)
 library(ggplot2)
+library(patchwork)
+library(ggupset)
 
 sq <- read.csv("data/nyc_squirrels.csv") %>% 
   suppressMessages()
+
+activity_cols <- c("running", "chasing", "climbing", "eating", "foraging")
+
+activity_df <- sq %>% select(activity_cols) %>% 
+  mutate(activity = pmap(across(where(is.logical)), 
+                         ~ names(c(...))[which(c(...))]))
+
+activity_upset <- ggplot(activity_df, aes(x=activity)) +
+  geom_bar() +
+  geom_text(stat='count', aes(label=after_stat(count)), vjust=-1, size=2.8) +
+  scale_x_upset(n_intersections = 20) +
+  xlab("Activities") + ylab("Counts") +
+  ylim(c(0, 1000)) +
+  theme_classic()
 
 behavior_cols <- c("kuks", "quaas", "moans",
                    "tail_flags", "tail_twitches")
@@ -14,11 +30,14 @@ behavior_df <- sq %>% select(all_of(behavior_cols)) %>%
 
 behavior_upset <- ggplot(behavior_df, aes(x=behavior)) +
   geom_bar() +
-  geom_text(stat='count', aes(label=after_stat(count)), vjust=-1, size=3) +
+  geom_text(stat='count', aes(label=after_stat(count)), vjust=-1, size=2.8) +
   scale_x_upset(n_intersections = 20) +
   xlab("Behaviors") + ylab("Counts") +
-  ylim(c(0, 2500)) +
+  ylim(c(0, 2600)) +
   theme_classic()
 
+patched_plt <- (activity_upset | behavior_upset) +
+  plot_annotation(tag_levels = 'A')
+
 ggsave(filename = "/home/rstudio/work/figures/behavior_upset.png",
-       behavior_upset, height = 4, width = 6)
+       patched_plt, height = 3, width = 10)
